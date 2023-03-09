db2cd2598e6b3cce46863c4c93ce080101af45da
sila.k <silakob2@gmail.com>
2023-03-09 13:42:21 +0700

    เพิ่ม Method Post ใน Hello Controller

--- src/main/java/com/registration/registration/RegistrationController.java
+++ src/main/java/com/registration/registration/RegistrationController.java
@@ -1,12 +1,22 @@
 package com.registration.registration;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class RegistrationController{
+public class RegistrationController {
+    public String helloString = "Hello World";
+    // #region
     @RequestMapping("/hello")
-    public String Hello(){
-        return "Hello World";
+    public String Hello() {
+        return helloString;
     }
+
+    @PostMapping("/hello")
+    public String UpdateHello(){
+        helloString = helloString.substring(helloString.length() -2, 1);
+        return helloString;
+    }
+    // #endregion
 }
